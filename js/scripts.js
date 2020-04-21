9f02ae8d1b7a71dbf767d6cdbaf5bae3da7a7915
me <me@me.me.com>
2020-04-21 15:32:07 -0700

    refactor mad libs according to 1.3.1.3

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,22 @@
 $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
-    var animal = $("#animal").val();
-    var celebrity = $("#celebrity").val();
-    var location = $("#location").val();
-    var food = $("#food").val();
+    var categories = ["animal","celebrity","location","food"];
+    categories.forEach(function(category){
+      var answer  = $("#"+ category).val();
+      $("." + category).text(answer);
+    }) 
+    
 
-    $(".animal").text(animal);
-    $(".celebrity").text(celebrity);
-    $(".location").text(location);
-    $(".food").text(food);
+    // var animal = $("#animal").val();
+    // var celebrity = $("#celebrity").val();
+    // var location = $("#location").val();
+    // var food = $("#food").val();
+
+    // $(".animal").text(animal);
+    // $(".celebrity").text(celebrity);
+    // $(".location").text(location);
+    // $(".food").text(food);
     $("#story").show();
   
   });
